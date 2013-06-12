3f6a30c5812265305b75412bc3306928c080b7c5
David Broomfield <dbroomfi@spsu.edu>
2013-06-12 07:43:43 -0400

    added some notes

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -22,6 +22,7 @@ class Welcome extends CI_Controller {
 		$this->load->view('defaultPageHeader');
                 $this->load->view('homeBody');
                 $this->load->view('defaultPageFooter');
+                //need to update menu
 	}
 }
 
